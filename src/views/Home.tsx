33c5bc655de0a19b92f5f6a2ff5f94dd9d66c1f9
suzeyu918 <213749686@qq.com>
2025-08-28 23:29:42 +0800

    Update Home.tsx

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -32,30 +32,12 @@ const Home: FC = () => {
             </svg>
             <span className="btn-text">全部接口</span>
           </button>
-          <button id="docs-button">
-            <svg
-              className="btn-icon"
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="currentColor"
-                d="M3 6c-.55 0-1 .45-1 1v13c0 1.1.9 2 2 2h13c.55 0 1-.45 1-1s-.45-1-1-1H5c-.55 0-1-.45-1-1V7c0-.55-.45-1-1-1m17-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2m-2 9h-8c-.55 0-1-.45-1-1s.45-1 1-1h8c.55 0 1 .45 1 1s-.45 1-1 1m-4 4h-4c-.55 0-1-.45-1-1s.45-1 1-1h4c.55 0 1 .45 1 1s-.45 1-1 1m4-8h-8c-.55 0-1-.45-1-1s.45-1 1-1h8c.55 0 1 .45 1 1s-.45 1-1 1"
-              />
-            </svg>
-            <span className="btn-text">项目文档</span>
-          </button>
         </div>
       </main>
       {html`
         <script>
           document.getElementById("all-button").addEventListener("click", () => {
             window.location.href = "/all";
-          });
-          document.getElementById("docs-button").addEventListener("click", () => {
-            window.open("https://blog.imsyy.top/posts/2024/0408");
           });
         </script>
       `}
